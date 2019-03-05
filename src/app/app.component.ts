b789ebd00a630d85eb64ef063bc31f304da05879
Gary Spillman <spinning.leak@gmail.com>
2019-03-05 13:53:28 -0600

    Updating To Do list widget, add style _auth="Z3NwaWxsbWFuOlR5cmVsbDE4NyE="

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'webdriver-gym';
+  title = 'webdriver-gym'
+  toDoArray = []
+  addToDo(value) {
+    if (value === '') {
+      console.log('Value cannot be null');
+    }
+    else {
+      this.toDoArray.push(value);
+      console.log(this.toDoArray);
+    }
+  }
+  deleteItem(todo) {
+    console.log('delete item');
+    for (let i=0 ;i<= this.toDoArray.length ;i++) {
+      if(todo==this.toDoArray[i]){
+        this.toDoArray.splice(i,1);
+      }
+    }
+  }
 }
